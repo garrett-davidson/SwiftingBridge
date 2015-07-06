24b8519c97f0b76f9076c4f79e76648c3f467468
Garrett Davidson <gdavids@purdue.edu>
2015-07-06 13:09:25 -0400

    Fixed issue with keyword escaping

--- SwiftingBridge.py
+++ SwiftingBridge.py
@@ -100,6 +100,7 @@ def isInterface(line):
 
 def parseType(type):
     type = type.rstrip("* ")
+    type = escapedName(type)
     
     typeDict = {
                 "id":                   "AnyObject",
@@ -130,7 +131,13 @@ def parseType(type):
         return type
 
 def escapedName(name):
-    reservedWords = ["as", "in", "for", "class", "deinit", "enum", "extension", "func", "import", "init", "internal", "let", "operator", "private", "protocol", "public", "static", "struct", "subscript", "typealias", "var", "break", "case", "continue", "default", "do", "else", "fallthrough", "for", "if", "in", "return", "switch", "where", "while", "as", "dynamicType", "false", "is", "nil", "self", "Self", "super", "true", "__COLUMN__", "__FILE__", "__FUNCTION__", "__LINE__", " associativity", "convenience", "dynamic", "didSet", "final", "get", "infix", "inout", "lazy", "left", "mutating", "none", "nonmutating", "optional", "override", "postfix", "precedence", "prefix", "Protocol", "required", "right", "set", "Type", "unowned", "weak", "willSet"]
+    
+    #Objective-C escaped keywords end in _
+    #We must un-escape them first
+    if name[-1:] == "_":
+        name = name[:-1]
+    
+    reservedWords = ["as", "in", "for", "class", "deinit", "enum", "extension", "func", "import", "init", "internal", "let", "operator", "private", "protocol", "public", "static", "struct", "subscript", "typealias", "var", "break", "case", "continue", "default", "do", "else", "fallthrough", "for", "if", "in", "return", "switch", "where", "while", "as", "dynamicType", "false", "is", "nil", "self", "Self", "super", "true", " associativity", "convenience", "dynamic", "didSet", "final", "get", "infix", "inout", "lazy", "left", "mutating", "none", "nonmutating", "optional", "override", "postfix", "precedence", "prefix", "Protocol", "required", "right", "set", "Type", "unowned", "weak", "willSet"]
     
     if name in reservedWords:
         name = "`" + name + "`"
